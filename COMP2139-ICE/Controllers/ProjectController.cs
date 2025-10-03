1149afe32b082bd3f3c9aa01c9b8b5f913902588
Tommy Shek <shek011713@gmail.com>
2025-10-03 12:13:18 -0400

    Lab4

--- COMP2139-ICE/Controllers/ProjectController.cs
+++ COMP2139-ICE/Controllers/ProjectController.cs
@@ -44,9 +44,68 @@ public class ProjectController : Controller
     public IActionResult Create(Project project)
 
     {
-        return RedirectToAction("Index");
+        if (ModelState.IsValid)
+        {
+            _context.Projects.Add(project);
+            _context.SaveChanges();
+             return RedirectToAction("Index");
+        }
+        return View(project);
     }
 
+    [HttpGet]
+    public IActionResult Edit(int id){
+        var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+        if (project == null)
+        {
+            return NotFound();
+        }
+        return View(project);
+    }
+
+    //Lab4 - Part3 - #2 
+
+    [HttpPost] 
+    [ValidateAntiForgeryToken] 
+    //         Bind("ProjectId  ,Name       ,Description")] Project project) 
+
+    public IActionResult Edit(int id, [Bind("ProjectId" ,"Name","Description")] Project project) 
+
+    { 
+
+        if (id != project.ProjectId) 
+        { 
+            return NotFound(); 
+        } 
+
+      //if (ModelState .IsValid) 
+
+        if (ModelState .IsValid) 
+        { 
+            try 
+            { 
+                _context.Projects.Update(project); 
+                _context.SaveChanges(); 
+            } 
+            catch (DbUpdateConcurrencyException) 
+            { 
+                if (!ProjectExists(project.ProjectId)) 
+                { 
+                    return NotFound(); 
+                } 
+                else 
+                { 
+                    throw; 
+                } 
+            } 
+            return RedirectToAction("Index"); 
+        } 
+        return View(project); 
+    } 
+    private bool ProjectExists(int id) 
+    { 
+        return _context.Projects.Any(e => e.ProjectId == id); 
+    } 
 
     [HttpGet]
     public IActionResult Details(int id)
@@ -59,6 +118,30 @@ public class ProjectController : Controller
             return NotFound();
         }
         return View(project);
+    }
+
+    [HttpGet] 
+    public IActionResult Delete(int id) 
+    {
+       var project = _context.Projects.FirstOrDefault(p => p.ProjectId == id);
+       if (project == null){
+            return NotFound();
+        }
+        return View(project);
+    }
+
+    [HttpPost ,ActionName("DeleteConfirmed")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var project = _context.Projects.Find(id);
+        _context.Projects.Remove(project);
+        _context.SaveChanges();
+        if(project != null)
+        {
+            return RedirectToAction("Index");
+        }
+        return NotFound();
     }
 
 
